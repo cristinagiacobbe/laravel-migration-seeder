a51943068f259049701c6786e1d58ffaf7919c10
cristinagiacobbe <crigiak@gmail.com>
2024-04-25 22:06:59 +0200

    insert records to trains_table from admin and add column with make-migrationd-add

--- database/migrations/2024_04_25_160012_create_trains_table.php
+++ database/migrations/2024_04_25_160012_create_trains_table.php
@@ -4,6 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/*
+## COLONNE-CAMPI DELLA TABELLA TRAINS
+Azienda
+Stazione di partenza
+Stazione di arrivo
+Orario di partenza
+Orario di arrivo
+Codice Treno
+Numero Carrozze
+In orario
+Cancellato
+*/
+
 return new class extends Migration
 {
     /**
@@ -13,6 +26,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('Azienda', 15);
+            $table->string('Departure_station', 50);
+            $table->string('Arrival_station', 50);
+            $table->time('Departure_time');
+            $table->time('Arrival_time');
+            $table->char('Train_code');
+            $table->smallInteger('Carriage');
+            $table->boolean('On_time');
+            $table->boolean('Deleted');
             $table->timestamps();
         });
     }
